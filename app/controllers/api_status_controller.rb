d350daf1a3097bfac9bc922bd9b3b277aa1c1fa6
Diego Costa <diecoscai@gmail.com>
2025-06-10 17:59:42 -0300

    Fix linting issues in controllers: string literals, whitespace, and spacing

--- app/controllers/api_status_controller.rb
+++ app/controllers/api_status_controller.rb
@@ -1,5 +1,5 @@
 class ApiStatusController < ApplicationController
-  skip_before_action :authorize_request, only: [:index]
+  skip_before_action :authorize_request, only: [ :index ]
 
   def index
     render json: {

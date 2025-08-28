ef2177f35d16dcc456965481a5997cc8864552a4
Fede <fcmde1995@gmail.com>
2025-08-28 18:43:41 -0300

    Remove conflicting serializers to fix exercise includes
    - Move ChallengeSerializer and ChallengeExerciseSerializer to backup
    - These serializers were interfering with include: syntax
    - Now challenges will use direct JSON like routines
    - Should properly include nested exercise data with include: { challenge_exercises: { include: :exercise } }

--- app/serializers/challenge_exercise_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ChallengeExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :exercise_id, :sets, :reps, :rest_time_seconds, :order_index, :notes
-  
-  belongs_to :exercise, serializer: ExerciseBasicSerializer
-end

6c4bca238e1d14bfffc3b47b543386c2b4e1cebc
Fede <fcmde1995@gmail.com>
2025-08-28 18:11:57 -0300

    Fix missing exercise_id in challenge_exercise serializer
    - Add exercise_id to ChallengeExerciseSerializer attributes
    - Now challenge_exercises JSON response includes exercise_id reference
    - Fixes issue where exercise reference was missing in API response

--- app/serializers/challenge_exercise_serializer.rb
+++ app/serializers/challenge_exercise_serializer.rb
@@ -1,5 +1,5 @@
 class ChallengeExerciseSerializer < ActiveModel::Serializer
-  attributes :id, :sets, :reps, :rest_time_seconds, :order_index, :notes
+  attributes :id, :exercise_id, :sets, :reps, :rest_time_seconds, :order_index, :notes
   
   belongs_to :exercise, serializer: ExerciseBasicSerializer
 end
